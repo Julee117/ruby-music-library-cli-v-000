73a0dacc94dc08b7db5a898dbfa9a8c0e5315368
Julianne Kim <juliannne@gmail.com>
2017-10-26 14:11:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,9 +10,9 @@ class Artist
     @songs = []
   end
 
-  def songs
-    @songs
-  end
+  # def songs
+  #   @songs
+  # end
 
   def add_song(song)
     songs << song unless self.songs.include?(song)
